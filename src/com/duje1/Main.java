886884ae7ae969f59af90cf574cab31921f0fb0b
Duje1 <duje1mandalinic@gmail.com>
2020-09-01 14:51:56 +0200

    got the frame centered and set size

--- src/com/duje1/Main.java
+++ src/com/duje1/Main.java
@@ -6,6 +6,10 @@ public class Main {
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("Don't get Rick Rolled");
-
+        frame.setSize(600,300);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setVisible(true);
     }
 }
