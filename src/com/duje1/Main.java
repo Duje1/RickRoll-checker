e3bb5b74ad099ecab8bc1c0a5c205e3eda528b1f
Duje1 <duje1mandalinic@gmail.com>
2020-09-01 14:44:27 +0200

    frame

--- src/com/duje1/Main.java
+++ src/com/duje1/Main.java
@@ -1,8 +1,11 @@
 package com.duje1;
 
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) {
+        JFrame frame = new JFrame("Don't get Rick Rolled");
 
     }
 }
