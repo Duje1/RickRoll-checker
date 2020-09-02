f8c12dac2fcbacdbabdb3a5bc429d2e3e919a149
Duje1 <duje1mandalinic@gmail.com>
2020-09-02 13:16:43 +0200

    Variables

--- src/com/duje1/Main.java
+++ src/com/duje1/Main.java
@@ -1,15 +1,20 @@
 package com.duje1;
 
 import javax.swing.*;
+import java.awt.image.ImageObserver;
 
 public class Main {
-
+    private static final String TITLE = "Rick Checker";
+    private static final int WIDTH=600,HEIGHT=300;
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Don't get Rick Rolled");
-        frame.setSize(600,300);
+
+        //initializing frame
+        JFrame frame = new JFrame(TITLE);
+        frame.setSize(WIDTH,HEIGHT);
         frame.setLocationRelativeTo(null);
         frame.setResizable(false);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setVisible(true);
+
     }
 }
